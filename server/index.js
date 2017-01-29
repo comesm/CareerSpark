19cafbbc373f3693f4c2c875c7fa9ec3c18b1c00
Michael <comesm@gmail.com>
2017-01-29 14:10:51 -0800

    Create scaffolding for project

---
+++ server/index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+//import our controller for db operations, also connects to DB
+var UserDbController = require('../db/controllers/User.js');
+
+app.use(bodyParser.json());
+
+
+//interact here with db or add routes elsewhere to keep code modularized
+//hypothetically, db operations would be delegated to these routes
+
+
+app.use('/', express.static(__dirname + '/../client/dist'));
+
+app.listen(3000, function() {
+  console.log('listening on port 3000');
+});
