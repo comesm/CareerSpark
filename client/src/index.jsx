99d02b76a1500213163ce21e7839c723b3c24c94
Joey Lee <jleepianist@gmail.com>
2017-01-31 11:44:31 -0800

    Updated index.jsx

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import User from './User.jsx';
+import red-x from '../'
 
 class App extends React.Component {
   constructor(props) {
