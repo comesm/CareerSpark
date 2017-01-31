72b82ef4f402ad4e0e33328301b816707a165db2
Joey Lee <jleepianist@gmail.com>
2017-01-31 11:35:12 -0800

    Fleshed out client scaffolding for app index

---
+++ client/src/UserPhoto.jsx
@@ -0,0 +1,7 @@
+export const UserPhoto = props => (
+  <img
+    img src="images/mickey-mouse.jpg"
+  	alt="enter dynamically-generated user or photo name on props argument here"
+  	id="user-photo"
+  />
+)
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,12 +1,32 @@
 import React from 'react';
 import { render } from 'react-dom';
+import User from './User.jsx';
 
-var App = () => (
+class App extends React.Component {
+  constructor(props) {
+  	super(props);
 
-<div>Supercharge Your Network with CareerSpark</div>
+  	this.state = {
 
-)
+  	}
+  }
 
 
+
+  render() {
+    return (
+  	  <div>
+  	    <header>
+  	      <div id="header">CareerSpark</div>
+          <div id="sub-header">Supercharge Your Professional Network</div>
+        </header>
+
+        <img src="images/red-x.png" alt="click to deny" id="red-x" />
+        <User />
+        <img src="images/green-check.png" alt="click to approve" id="green-check" />
+      </div>
+    )
+  }
+}
 
 render(<App />, document.getElementById('app'));
