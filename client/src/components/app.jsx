7371edb22ecde3e0b6d57fda1120d7ddfd559d6b
Michael <comesm@gmail.com>
2017-01-31 19:27:22 -0800

    finish testing

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -20,12 +20,12 @@ export default class App extends React.Component {
         </header>
 
         <img src="./images/red-x.png"
-             alt="click to reject" 
+             alt="click to reject"
              className="user-choice red-x"
         />
         <User />
-        <img src="./images/green-check.png" 
-             alt="click to approve" 
+        <img src="./images/green-check.png"
+             alt="click to approve"
              className="user-choice green-check"
         />
       </div>
