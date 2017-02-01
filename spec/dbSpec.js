00438da09bff26449f133d55066cd9d12c59e17a
Michael <comesm@gmail.com>
2017-01-31 17:19:26 -0800

    tests pass

--- spec/dbSpec.js
+++ spec/dbSpec.js
@@ -122,7 +122,6 @@ describe('Users', function() {
             });
         });
       });
-
       it('should delete user', function(done) {
 
           controller.getUserId('comesm', function(id) {
@@ -166,10 +165,21 @@ describe('Users', function() {
          });
       });
     });
-<<<<<<< 025107efb62c3586d0310cb0c72962ac35a82683
-=======
 
->>>>>>> rebase conflict merge
+      it('should delete user', function(done) {
+
+          controller.getUserId('comesm', function(id) {
+              controller.deleteUser(id, function() {
+                controller.findAllUsers(function(results) {
+                  console.log('136------', results)
+                 expect(results.length).to.equal(1);
+                 expect(results[0].dataValues.username).to.equal('smartPerson12');
+                 done();
+                })
+              });
+            });
+        });
+    });
   });
 
 
