b1408887dbed580fbb4cd21c2c6d7fe0b9aa77c7
Joey Lee <jleepianist@gmail.com>
2017-02-03 09:19:30 -0800

    (client) minor styling aditions

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -3,11 +3,24 @@ import { Link } from 'react-router';
 
 export default class Nav extends React.Component {
   render() {
+
   	return (
   	  <ul id="Nav">
-        <li><Link to="/" activeClassName="active">Home</Link></li>
-        <li><Link to="/profile" activeClassName="active">Profile</Link></li>
-        <li><Link to="/about" activeClassName="active">About</Link></li>
+        <li>
+          <Link to="/" 
+                activeClassName="active">Home
+          </Link>
+        </li>
+        <li>
+          <Link to="/profile" 
+                activeClassName="active">Profile
+          </Link>
+        </li>
+        <li>
+          <Link to="/about" 
+                activeClassName="active">About
+          </Link>
+        </li>
       </ul>
   	)
   }
