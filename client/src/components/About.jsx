c9b611eeff5e4973b9452aef5f257bb2b7ca7537
Joey Lee <jleepianist@gmail.com>
2017-02-03 09:24:29 -0800

    (git) Fixing merge conflict - no files changed

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import Header from './Header.jsx';
 
-export default class About extends React.Component {
+export default class Profile extends React.Component {
   render() {
-    return <Header />
+  	return <Header />
   }
 }
-
