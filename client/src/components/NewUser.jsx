b9775a27f4e8ec1df1099592255b35903267d11b
Greg Coffeng <gcoffeng@gmail.com>
2017-02-02 22:04:44 -0800

    (feat) Changed the user input of 'field' from text input to a pull down menu of selected careers. This way we can more easily make associations between users because they will self select from a limited number of categories.

--- client/src/components/NewUser.jsx
+++ client/src/components/NewUser.jsx
@@ -60,6 +60,10 @@ export default class NewUser extends React.Component {
     })
   }
 
+	handleChange(event) {
+    this.setState({field: event.target.field});
+  }
+
 	handleInputChange(event) {
     const target = event.target;
     const value = target.value;
