ece02fa3229271fdd80f3ae550bfefa6eec6a36d
Greg Coffeng <gcoffeng@gmail.com>
2017-02-05 13:11:53 -0800

    One more file to commit for adding Login page

--- client/src/components/SignUp.jsx
+++ client/src/components/SignUp.jsx
@@ -3,9 +3,6 @@ import NewUser from './NewUser.jsx';
 import Header from './Header.jsx';
 import Nav from './Nav.jsx';
 
-
-// import $ from 'jquery';
-
 export default class SignUp extends React.Component {
   constructor(props) {
   	super(props);
